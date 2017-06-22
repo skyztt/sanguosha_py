bc8181fedc67da25f45b30b95943a8384a384f85
zhangtao <zhangtao@graphstrategist.com>
2017-06-22 20:03:11 +0800

    添加生成的工程文件及翻译文件（提交翻译文件是为了pylupdate5.exe能够增量更新翻译文件）

---
+++ sanguosha_py.pro
@@ -0,0 +1,7 @@
+SOURCES = src\Button.py\
+          src\main.py\
+          src\MainWindow.py\
+          src\Pixmap.py\
+          src\Settings.py\
+          src\StartScene.py
+TRANSLATIONS = sanguosha_py.ts
---
+++ sanguosha_py.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="zh_CN" sourcelanguage="">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="src/MainWindow.py" line="18"/>
+        <source>Game</source>
+        <translation>游戏</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.py" line="20"/>
+        <source>Start Game</source>
+        <translation>开始游戏</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.py" line="22"/>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.py" line="25"/>
+        <source>Tools</source>
+        <translation>工具</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.py" line="26"/>
+        <source>Configure</source>
+        <translation>配置</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.py" line="28"/>
+        <source>Start Server</source>
+        <translation>启动服务器</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.py" line="30"/>
+        <source>General Preview</source>
+        <translation>武将一览</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.py" line="33"/>
+        <source>Help</source>
+        <translation>帮助</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.py" line="34"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.py" line="36"/>
+        <source>Acknowledgement</source>
+        <translation>鸣谢</translation>
+    </message>
+</context>
+</TS>
